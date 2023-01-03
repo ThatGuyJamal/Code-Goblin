1713313bb5ee712b035588161e17d656b701a652
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-01-03 12:16:03 -0500

    debug event

---
+++ src/events/event_debug.ts
@@ -0,0 +1,8 @@
+import type { Client } from 'oceanic.js';
+import { isCanary } from '../config/config.js';
+
+export default async function debugEvent(this: Client, info: string, id?: number) {
+	if(isCanary) {
+        console.debug(`[Debug${id === undefined ? '' : `/${id}`}]:`, info);
+    }
+}
