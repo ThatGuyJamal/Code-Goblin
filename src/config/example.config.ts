c54aba3427ea9eeda881a026ef9c847ed731b2e1
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-01-16 22:38:21 -0500

    Update example.config.ts

--- src/config/example.config.ts
+++ src/config/example.config.ts
@@ -6,18 +6,17 @@ export default {
 	BotClientId: '1055671501870874634',
 	BotClientOAuth2Url:
 		'https://discord.com/api/oauth2/authorize?client_id=1055671501870874634&permissions=2684700800&scope=bot%20applications.commands',
-	BotName: 'Whisper Room',
+	BotName: 'Code Buddy',
 	GithubRepository: 'https://github.com/whisper-room-dev',
 	GithubContributionUrl: 'https://github.com/whisper-room-dev/.github/blob/main/contributing.md',
 	DevelopmentServerId: isCanary
 		? ['']
 		: [
-				// Whisper Room Dev
+				// Support Server
 				'991449362246934648'
 		  ],
 	DevelopmentServerInviteUrl: 'https://discord.gg/MSTrBrNaGn',
 	IsInDevelopmentMode: true,
-	BotPrefix: 'wr!',
 	BotApiLogChannelId: isCanary ? '' : '1056292297756639342',
 	BotErrorLogChannelId: isCanary ? '' : '1056339397194297384',
 	SuperUsers: [
