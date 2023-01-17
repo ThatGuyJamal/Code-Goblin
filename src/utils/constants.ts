405825cf3095dc4bd1e5191822221f883472f18b
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-01-17 00:09:12 -0500

    Update constants.ts
    change embed colors

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,26 +9,15 @@ export default {
 				commandDisabled: `This command is currently disabled.`,
 				botPermissions: `I do not have the required permissions to run this command.`,
 				userPermissions: `You do not have the required permissions to run this command.`,
-				isOnCooldown: `You are currently on cooldown for this command.`
 			}
 		},
-		commands: {
-			info: {
-				company: {
-					bio: 'A place for people to talk about anything they want, without being judged. Users are anonymous, and can only see the messages of other users. The messages are deleted after a certain amount of time, so you can talk about anything you want, without worrying about it being seen by anyone else.'
-				},
-				bot: {
-					bio: `An open source tool helpful for programmers on discord. While its main functions are support the Whisper Rooms API, it can also be used for programmers to automate their own development.`
-				}
-			}
-		}
 	},
 	numbers: {
 		// Used in embeds
 		colors: {
-			primary: 0x8b4513,
-			secondary: 0xa0522d,
-			tertiary: 0xd2691e
+			primary: 0x28cea3,
+			secondary: 0x28a6ce,
+			tertiary: 0x2853ce
 		}
 	}
 };
